564b779e5ea2c0e808c88fc9f67c57512d98f0ad
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2024-03-06 22:42:04 +0100

    Modify the Notifications

--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -6,4 +6,9 @@ export function getFullYear() {
 export function getFooterCopy(isIndex) {
     return isIndex ? 'Holberton School' : 'Holberton School main dashboard';
   }
+
+export function getLatestNotification() {
+  return '<strong>Urgent requirement</strong> - complete by EOD';
+}
+
   
