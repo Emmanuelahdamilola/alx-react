eaf833fd8bbab30fcf986d27813d931c38a7d575
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2024-03-07 01:19:36 +0100

    Create a project using Webpack

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+      static: './dist',
+      hot: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './dist/index.html',
+      }),
+    ],
+    devtool: 'inline-source-map', 
+};
